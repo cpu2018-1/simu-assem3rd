0e2286e37b2d94f62e036fd3bb78684b3961cb9e
cpu2018-1 <apapupx0405@gmail.com>
2018-12-08 15:11:55 +0900

    floatのc実装を3rdの仕様に合わせて変更

--- simulator/float.h
+++ simulator/float.h
@@ -5,13 +5,13 @@
 float fadd(float a, float b);
 float fsub(float a, float b);
 float fmul(float a, float b);
-float fdiv(float a, float b);
+float finv(float f);
+float fsqrt(float f);
 
 int feq(float a, float b);
 int flt(float a, float b);
 int fle(float a, float b);
 
-float fsqrt(float f);
 float fneg(float f);
 float itof(int i);
 int ftoi(float f);
